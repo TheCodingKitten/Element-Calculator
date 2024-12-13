73e767215ce77b092c59482c4516fe2bbb90624f
TheCodingCat <hnevilluk@gmail.com>
2024-12-13 15:24:01 +0000

    Import Elements and Symbols form Element_Calculator_CLI.py and add error handling for value too low

--- EC_ShellConfigs.py
+++ EC_ShellConfigs.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
+# 
+# Element Shell Configurations
+# https://github.com/TheCodingKitten/Element-Calculator
+# 
 
-#Element Calculator
+from Element_Calculator_CLI import Elements
+from Element_Calculator_CLI import Symbols
 
-#Element tuples
-Elements = ["NULL", "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tennessine", "Oganesson"]
-Symbols = ["NULL" "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"]
-Numbers = ["NULL", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118"]
-
-numberInput = int(input("Enter Atomic Number of an element: ")) #User input to numberInput variable
-if numberInput > 118: #Checks if atomic number is greater than 118 and closes script
-    print("Error: Input too high")
+numberInput = int(input("Enter Atomic Number of an element: ")) # User input to numberInput variable
+if numberInput < 1 or numberInput > 118: # Checks if atomic number is greater than 118 and closes script
+    print("Error: Invalid input. Input must be between 1 and 118.")
     exit()
 
 #Variables
-num2 = numberInput - 2 #Remainder after shell 1
-num3 = num2 / 8 #Number of shells
-num4 = num2 % 8 #Remaining electrons in outer most shell
-num5 = str(num4) #String of num4
+num2 = numberInput - 2 # Remainder after shell 1
+num3 = num2 / 8 # Number of shells
+num4 = num2 % 8 # Remaining electrons in outer most shell
+num5 = str(num4) # String of num4
 
-#Print info about element
+# Print info about element
 print("Element Name: ", Elements[numberInput],"\n" "Element Symbol: ", Symbols[numberInput], "\n" "Shell Configuration: ")
-if numberInput <= 2: #Full inner shell only
+if numberInput <= 2: # Full inner shell only
     print(numberInput)
 else: 
-    if num4 == 0: #Full inner, full middle(s), full outer shells
+    if num4 == 0: # Full inner, full middle(s), full outer shells
         print("2", int(num3)*"8 ")
-    else: #Full inner, full middle(s), not full outer shells
+    else: # Full inner, full middle(s), not full outer shells
         print("2", int(num3)*"8 ", end=num5)
